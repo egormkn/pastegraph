1af9955fbd6dd0f66b19ae4039dd2c5c146ea9ef
Maxim Averin <amax0703@gmail.com>
2016-09-22 12:50:45 +0300

    Build graph by quick. Didn't work

--- js/example.js
+++ js/example.js
@@ -6,19 +6,18 @@ var node2 = graph.newNode({label: '2'});
 var node3 = graph.newNode({label: '3'});
 var node4 = graph.newNode({label: '4'});
 
-graph.newEdge(node1, node2, {color: '#00A0B0'});
-graph.newEdge(node1, node4, {color: '#6A4A3C'});
+graph.newEdge(node1, node2, {label: "GGGG"});
+graph.newEdge(node1, node4, {label: "GGGG"});
 graph.newEdge(node2, node1, {color: '#CC333F'});
 graph.newEdge(node2, node3, {color: '#EB6841'});
 graph.newEdge(node2, node4, {color: '#EDC951'});
 graph.newEdge(node3, node2, {color: '#7DBE3C'});
 
-
-jQuery(function(){
-var springy = window.springy = jQuery('#springydemo').springy({
-  graph: graph,
-  nodeSelected: function(node){
-    console.log('Node selected: ' + JSON.stringify(node.data));
-  }
-});
+jQuery(function () {
+    var springy = window.springy = jQuery('#springydemo').springy({
+        graph: graph,
+        nodeSelected: function (node) {
+            console.log('Node selected: ' + JSON.stringify(node.data));
+        }
+    });
 });
--- js/graphlogic.js
+++ js/graphlogic.js
@@ -5,10 +5,12 @@
  TODO
  1. [DONE] convert text from textarea to graph
  2. build springy graph
+ 2.1 build directional or inderectional graph
  3. make cool form with checked properties and so on
  4. localstorage
  5. make convert versions
- 6. make iisues form pavlenko
+ 6. make issues form pavlenko
+ 7. how to store information and variables
  */
 
 $(document).ready(function () {
@@ -23,14 +25,42 @@ $(document).ready(function () {
 
     var textBlock = $("#examples");
     var matrix = textTo2DArray();
-    /* print_matrix
-    for (var i = 0; i < matrix.length; i++) {
-        var line = "<p>";
-        for (var j = 0; j < matrix[i].length; j++) {
-            line += matrix[i][j] + " ";
-        }
-        line += "</p>";
-        textBlock.append(line);
+    var drawingGraph = window.spingy;
+
+    var buildGraph = function () {
+        var graph = new Springy.Graph();
+
+        var node1 = graph.newNode({label: '1'});
+        var node2 = graph.newNode({label: '2'});
+        var node3 = graph.newNode({label: '3'});
+        var node4 = graph.newNode({label: '4'});
+
+        graph.newEdge(node1, node2, {label: "GGGG"});
+        graph.newEdge(node1, node4, {label: "GGGG"});
+        graph.newEdge(node2, node1, {color: '#CC333F'});
+        graph.newEdge(node2, node3, {color: '#EB6841'});
+        graph.newEdge(node2, node4, {color: '#EDC951'});
+        graph.newEdge(node3, node2, {color: '#7DBE3C'});
+        return graph;
     }
-    */
+
+
+    $("#refresh").click(function () {
+        var buildedGraph = buildGraph();
+        if(buildedGraph !== undefined){
+            drawingGraph = $("#springydemo").springy({graph: buildedGraph});
+        }
+    });
+
+
+    /* print_matrix
+     for (var i = 0; i < matrix.length; i++) {
+     var line = "<p>";
+     for (var j = 0; j < matrix[i].length; j++) {
+     line += matrix[i][j] + " ";
+     }
+     line += "</p>";
+     textBlock.append(line);
+     }
+     */
 })
