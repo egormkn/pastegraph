0698a1726a4ef54f87400513ca185fdaa9da4039
Maxim Averin <amax0703@gmail.com>
2016-10-12 18:44:48 +0300

    made deletion Node without edges

--- js/graphlogic.js
+++ js/graphlogic.js
@@ -6,18 +6,21 @@
  1.  [DONE]  convert text from textarea to graph
  2.  [DONE]  build springy graph
  2.1 [DONE] build directional or inderectional graph
- 3. make cool form with checked properties and so on
- 3.1 [fixed] bug!!!! the edges are doubled
- 3.1 three ways of graph
- 4. localstorage
+ 3.  make cool form with checked properties and so on
+ 3.1 [DONE] bug!!!! the edges are doubled
+ 3.2 [DONE] detach Node in good manner (when no edges connect with it)
+ 4. three ways of graph
+ 4.1 redraw graph
+
+
+ 5. localstorage
  5. make good form with submit
- 5. make convert versions
  6. make issues form pavlenko
- 1. detach Node in good manner(when no edges connect with it)
  7. how to store information and variables
+ 8. strange bugs and appear of old version of graph
+ 9. mocha tests
 
  Final verison on the end of the week: nice design, fix any bugs, 3 ways, localstorqage
- mocha tests
  */
 
 // console.log(currentElement.source.id, currentElement.target.id);
@@ -45,11 +48,18 @@ $(document).ready(function () {
     }
 
 
-
     var drawingGraph;
 
     var buildGraph = function () {
         var graph = new Springy.Graph();
+        //Springy.Renderer.drawEdge.arrowWidth  = 0;
+       // Springy.Renderer.drawEdge.arrowHeight = 0;
+        if($("input[type='radio'][name='edgeList']").is(":checked"))
+            drawEdgeList()
+        if($("input[type='radio'][name='edgeList']").is(":checked"))
+            drawEdgeList()
+        if($("input[type='radio'][name='edgeList']").is(":checked"))
+            drawEdgeList()
 
         var isDirectional = $('#directional').is(':checked');
         var matrix = textTo2DArray();
@@ -60,17 +70,22 @@ $(document).ready(function () {
         for (var i = numeration; i <= vertices; i++)
             graph.addNodes(i);
 
+        var used = new Array(vertices);
+        used.fill(0);
         for (var i = 0; i < matrix.length; i++) {
             for (var j = 0; j < matrix[i].length; j++) {
                 if (matrix[i][j] == 1) {
                     var array = graph.edges;
 
-                     if (!findEdge(array, i + 1, j + 1)) graph.addEdges([i + 1, j + 1]);
-
+                    if (!findEdge(array, i + 1, j + 1)) {
+                        graph.addEdges([i + 1, j + 1]);
+                    }
 
                     if (isDirectional) {
 
-                        if (!findEdge(array, j + 1, i + 1)) graph.addEdges([j + 1, i + 1]);
+                        if (!findEdge(array, j + 1, i + 1)) {
+                            graph.addEdges([j + 1, i + 1]);
+                        }
 
                     }
                 }
@@ -78,9 +93,20 @@ $(document).ready(function () {
 
         }
 
+        for (var i = 0; i < matrix.length; i++)
+            for (var j = 0; j < matrix[i].length; j++)
+                if (matrix[i][j] == 1) {
+                    used[i] = 1;
+                    used[j] = 1;
+                }
+        for(var i = 0; i < used.length; i++)
+            if(used[i] == 0) graph.removeNode(graph.nodes[i]);
         return graph;
     };
 
+    function graph_to_edge_list(graph){
+
+    };
 
     $("#refresh").click(function () {
         var buildedGraph = buildGraph();
