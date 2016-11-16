cfb0f79a2c829db9cda3c5749258041e0b76a6c7
Maxim Averin <amax0703@gmail.com>
2016-11-16 12:37:21 +0300

    add bit.ly

--- js/ui.js
+++ js/ui.js
@@ -1,4 +1,14 @@
+//TODO bit.ly; add yandex.metrika stats; redisign
+
+
 $(document).ready(function () {
+
+    /*
+     algorithm: at first we try to parse link and then try to build
+     graph from textarea
+     */
+    parse_link();
+
     function clean_errors() {
         $('#errors').hide();
         $('#errors').text('');
@@ -16,77 +26,23 @@ $(document).ready(function () {
         else if ($("input[type='radio'][value='matrix']").is(":checked")) {
             $("textarea#graph-source").val(graphToMatrix(old_graph));
         }
+        update_link();
     });
 
     $("input[type='checkbox']").change(function () {
-        $("#links").text(update_link());
+        update_link();
         $("#refresh").click();
     });
 
     $("#refresh").click(function () {
         clean_errors();
         buildedGraph = buildGraph();
-        $("#links").text(update_link());
-        parse_link();
+        update_link();
         $('#errors').show();
         if (buildedGraph !== undefined) {
             drawingGraph = $("#springydemo").springy({graph: buildedGraph});
         }
     });
-
-    var textarea = document.getElementById('graph-source');
-    var directionalInput = document.getElementById('directional');
-
-    if (!sessionStorage.getItem('graph')) {
-        populateStorage();
-    } else {
-        setText();
-    }
-
-    function populateStorage() {
-
-        sessionStorage.setItem('graph', $('#graph-source').val().trim());
-        sessionStorage.setItem('directed', $('#directional').is(':checked'));
-        var method;
-        $("input[type='radio']").each(function () {
-            if ($(this).is(':checked')) {
-                method = this.value;
-            }
-        });
-        sessionStorage.setItem('method', method);
-    }
-
-    function setText() {
-
-        var graphText = sessionStorage.getItem('graph');
-        var directionalValue = sessionStorage.getItem('directed');
-        var method = sessionStorage.getItem('method');
-
-
-        if (directionalValue == "true")
-            $('#directional').prop('checked', true);
-        else
-            $('#directional').prop('checked', false);
-
-
-        $('#directional').prop('checked', directionalValue);
-        $("input[type='radio']").each(function () {
-            if (this.value == method) {
-                $(this).prop("checked", true);
-            }
-        });
-        document.getElementById('graph-source').value = graphText;
-        $('refresh').click();
-    }
-
-
-    directionalInput.onchange = populateStorage;
-    textarea.onchange = populateStorage;
-
-    $("input[type='radio']").change(function () {
-        populateStorage();
-    });
-
 
     // draw graph immediately when page is opened
     $("#refresh").click();
@@ -107,33 +63,33 @@ $(document).ready(function () {
         if (arrayArgs[1].substr(0, 8) === "digraph{") {
             directed = true;
             arrayArgs[1] = arrayArgs[1].slice(8);
-        } else if(arrayArgs[1].substr(0, 6) === "graph{"){
+        } else if (arrayArgs[1].substr(0, 6) === "graph{") {
             directed = false;
             arrayArgs[1] = arrayArgs[1].slice(6);
-        } else{
+        } else {
             return "";
         }
         arrayArgs[1] = arrayArgs[1].substr(0, arrayArgs[1].length - 8);
         var edges = arrayArgs[1].split(",");
-        for(var i = 0; i < edges.length; i++)
-            edges[i] = edges[i].replace("-", ' ');
+        for (var i = 0; i < edges.length; i++)
+            edges[i] = edges[i].replace("-", '');
 
         if (directed == "true")
             $('#directional').prop('checked', true);
         else
             $('#directional').prop('checked', false);
+        isDirectional = $('#directional').is(':checked');
 
-        var stringEdges = edges.join('\n');
-        console.log(stringEdges);
-        $("textarea#graph-source").val(stringEdges);
-        var graph = getEdgeList(textTo2DArray(stringEdges));
-
-        if (method == "edgeList"){
+        var graph = getEdgeList(edges);
+        if (method == "edgeList") {
+            $("textarea#graph-source").val(graphToEdgeList(graph));
             $("input[type='radio'][value='edgeList']").prop("checked", true);
-        } else if(method == "adjacencyList"){
-            $("input[type='radio'][value='edgeList']").prop("checked", true);
-        } else if(method == "matrix"){
-            $("input[type='radio'][value='edgeList']").prop("checked", true);
+        } else if (method == "adjacencyList") {
+            $("textarea#graph-source").val(graphToAdjacencyList(graph));
+            $("input[type='radio'][value='adjacencyList']").prop("checked", true);
+        } else if (method == "matrix") {
+            $("textarea#graph-source").val(graphToMatrix(graph));
+            $("input[type='radio'][value='matrix']").prop("checked", true);
         }
 
 
@@ -157,4 +113,21 @@ $(document).ready(function () {
         var finalLink = (isDirectional ? "digraph" : "graph") + "{" + result.join(",") + "}" + "&method=" + method;
         history.pushState({query: finalLink}, "graphpage", "?q=" + finalLink)
     }
+
+    $('#get_link').click(function () {
+        var xhr = new XMLHttpRequest();
+        var requestURL = "https://api-ssl.bitly.com/v3/shorten?access_token=e39789de6e564128360fef76d31b84f82b0865f9&longUrl="
+        var currentURL = window.location.href + "&format=txt";
+        xhr.open("GET", requestURL + currentURL, true);
+        xhr.send();
+        xhr.onreadystatechange = processRequest;
+        function processRequest(e) {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                var response = xhr.responseText;
+                link = response;
+            }
+            $("#short-link").text(link);
+            $("#short-link").attr("href", link);
+        }
+    });
 });
